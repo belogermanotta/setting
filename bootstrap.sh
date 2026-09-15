78aa2e18a2633dd8e98fea70a3a438ee441464c0
Kobe Germanotta <nikephoros.d.phokas@gmail.com>
2026-09-14 20:46:50 -0400

    Add herdr config

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,6 +87,7 @@ done
 link "$REPO_DIR/ide/nvimlinux/nvim" "$HOME/.config/nvim"
 link "$REPO_DIR/ide/terminal/wezterm/weztermlinux.lua" "$HOME/.config/wezterm/wezterm.lua"
 link "$REPO_DIR/dotenv/.config/hypr" "$HOME/.config/hypr"
+link "$REPO_DIR/dotenv/.config/herdr/config.toml" "$HOME/.config/herdr/config.toml"
 
 # --- 5. Obsidian settings (sanitized snapshot -> vault) -------------------------
 VAULT_DIR="${VAULT_DIR:-$HOME/notes}"
